1f289745887d2ef7ddaa15e6dd4861939ee3b1c1
Jeremias <jnahuel46@gmail.com>
2024-11-01 01:24:10 -0300

    add validation with Zod

--- app/(pages)/details/page.tsx
+++ app/(pages)/details/page.tsx
@@ -1,54 +1,158 @@
-import {Button} from "@/components/Button";
+'use client'
+import { Button } from "@/components/Button";
 import CommonCard from "@/components/CommonCard";
 import PagesLayout from "@/components/PagesLayout";
-import {TitlePage} from "@/components/TitlePage";
+import { TitlePage } from "@/components/TitlePage";
+import { useState } from "react";
+import { z } from "zod";
+import ErrorMessage from "@/components/ErrorMessage";
+
+const formSchema = z.object({
+    name: z
+      .string()
+      .min(1, "El nombre es requerido")
+      .max(50, "El nombre no puede exceder los 50 caracteres")
+      .regex(/^[A-Za-z\s]+$/, "El nombre solo puede contener letras y espacios"),
+    
+    email: z
+      .string()
+      .min(1, "El correo electrónico es requerido")
+      .email("El correo electrónico es inválido"),
+  
+    phone: z
+      .string()
+      .min(1, "El teléfono es requerido")
+      .regex(/^\d+$/, "El teléfono solo puede contener números")
+});
 
 export default function DetailsPage() {
-    return (
-        <PagesLayout>
-            <TitlePage title="Mis datos"/>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-            <CommonCard>
-                <form className="flex flex-col gap-[24px]">
-                    <div className="mt-[10px] h-[65px]">
-                        <label
-                            className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
-                            Nombre <span className="text-customRed">*</span>
-                        </label>
-                        <input
-                            required
-                            className="w-full h-[45px] mt-1 pl-[18px] border border-gray-300 rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px"
-                            placeholder="Nombre"
-                        />
-                    </div>
-                    <div className="h-[65px]">
-                        <label
-                            className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
-                            Email <span className="text-customRed">*</span>
-                        </label>
-                        <input
-                            type="email"
-                            required
-                            className="w-full h-[45px] mt-1 pl-[18px] border border-gray-300 rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px"
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="h-[65px]"><label
-                        className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
-                        Teléfono <span className="text-customRed">*</span>
-                    </label>
-                        <input
-                            type="tel"
-                            required
-                            className="w-full h-[45px] mt-1 pl-[18px] border border-gray-300 rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px"
-                            placeholder="Teléfono"
-                        />
-                    </div>
-                    <div className="mt-[16px] mb-[18px]">
-                        <Button title="Guardar"/>
-                    </div>
-                </form>
-            </CommonCard>
-        </PagesLayout>
-    );
+  const [errors, setErrors] = useState<{
+    name?: string;
+    email?: string;
+    phone?: string;
+  }>({});
+
+  const handleSave = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrors({}); // Limpiar errores antes de la validación
+
+    const newErrors: { name?: string; email?: string; phone?: string } = {}; // Usar 'const' aquí
+
+    // Validación del nombre
+    try {
+      formSchema.shape.name.parse(name);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        newErrors.name = error.errors[0]?.message; // Guardar solo el primer error
+      }
+    }
+
+    // Validación del correo
+    try {
+      formSchema.shape.email.parse(email);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        newErrors.email = error.errors[0]?.message;
+      }
+    }
+
+    // Validación del teléfono
+    try {
+      formSchema.shape.phone.parse(phone);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        newErrors.phone = error.errors[0]?.message;
+      }
+    }
+
+    // Actualizar el estado de errores solo si hay nuevos errores
+    setErrors(newErrors);
+
+    // Si no hay errores, proceder con la lógica de guardado
+    if (Object.keys(newErrors).length === 0) {
+      // Aquí puedes hacer lo que necesites con los datos válidos
+      console.log("Datos válidos:", { name, email, phone });
+    }
+  };
+
+  return (
+    <PagesLayout>
+      <TitlePage title="Mis datos" />
+
+      <CommonCard>
+        <form className="flex flex-col gap-[21px]" onSubmit={handleSave}>
+          <div>
+            <label className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
+              Nombre <span className="text-customRed">*</span>
+            </label>
+
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className={`w-full h-[45px] mt-1 pl-[18px] border rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px ${
+                errors.name ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Nombre"
+            />
+            {errors.name && (
+              <ErrorMessage
+                message={errors.name}
+                onClose={() => setErrors({ ...errors, name: undefined })}
+              />
+            )}
+          </div>
+
+          <div>
+            <label className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
+              Email <span className="text-customRed">*</span>
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className={`w-full h-[45px] mt-1 pl-[18px] border rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Email"
+            />
+            {errors.email && (
+              <ErrorMessage
+                message={errors.email}
+                onClose={() => setErrors({ ...errors, email: undefined })}
+              />
+            )}
+          </div>
+
+          <div >
+            <label className="text-customBlack font-open-sans text-14px font-normal leading-19px text-left">
+              Teléfono <span className="text-customRed">*</span>
+            </label>
+            <input
+              type="tel"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className={`w-full h-[45px] mt-1 pl-[18px] border rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px ${
+                errors.phone ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Teléfono"
+            />
+            {errors.phone && (
+              <ErrorMessage
+                message={errors.phone}
+                onClose={() => setErrors({ ...errors, phone: undefined })}
+              />
+            )}
+          </div>
+
+          <div className="mt-[14px] mb-[16px]">
+            <Button title="Guardar" />
+          </div>
+        </form>
+      </CommonCard>
+    </PagesLayout>
+  );
 }
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "./Button";
 import { useTodoStore } from "@/store/todoStore";
+import { z } from "zod";
+import ErrorMessage from "./ErrorMessage"; // Importar el nuevo componente
+
+const taskSchema = z.object({
+  title: z.string().min(1, "El nombre es requerido"),
+  description: z
+    .string()
+    .max(200, "La descripción no puede exceder 200 caracteres")
+    .min(1, "La descripcion es requerida"),
+});
 
 interface ModalProps {
   isOpen: boolean;
@@ -12,53 +22,107 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   const [description, setDescription] = React.useState("");
   const { addTodo } = useTodoStore();
 
+  // State for error handling
+  const [errors, setErrors] = React.useState<{
+    title?: string;
+    description?: string;
+  }>({});
+
+  // Reset state when modal opens
+  useEffect(() => {
+    if (isOpen) {
+      setName("");
+      setDescription("");
+      setErrors({});
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   const handleSave = async () => {
-    const newTask = {
-      id: Math.random(), // Genera un ID único, puedes cambiar esto si necesitas un ID específico
-      title: name,
-      description: description,
-      completed: false,
-      userId: 1,
-    };
+    try {
+      taskSchema.parse({ title: name, description });
 
-    await addTodo(newTask); // Agrega la tarea a través del store
-    onClose(); // Cierra el modal
-    setName(""); // Limpia el input
-    setDescription(""); // Limpia la descripción
+      const newTask = {
+        id: Math.random(),
+        title: name,
+        description: description,
+        completed: false,
+        userId: 1,
+      };
+
+      await addTodo(newTask);
+      onClose();
+      setErrors({}); // Clear errors on success
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErrors = error.flatten().fieldErrors;
+        setErrors(fieldErrors); // Set validation errors
+      }
+    }
   };
 
   return (
     <div className="fixed inset-0 pt-[27px] flex items-start justify-center z-50 bg-[#2C2C2CC2] backdrop-blur-md">
-      <div className="bg-white rounded-lg shadow-lg p-4 w-96 relative">
+      <div className="bg-white rounded-lg shadow-lg p-4 w-[90%] max-w-[600px] relative">
+        {/* Botón para cerrar el modal */}
         <button
           onClick={onClose}
           className="absolute top-2 right-2 font-semibold text-gray-500 hover:text-gray-700"
+          aria-label="Close modal" // Agregar accesibilidad
         >
-          {/* SVG para cerrar el modal */}
+          <svg
+            className="fill-current h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+          </svg>
         </button>
-        <h2 className="text-lg font-semibold pt-[8px] mb-[32px]">Añadir Tarea</h2>
+
+        <h2 className="text-lg font-semibold pt-[8px] mb-[32px]">
+          Añadir Tarea
+        </h2>
+
         <label className="text-customGrey font-open-sans text-14px font-normal leading-19px text-left">
-          Nombre
+          Nombre{" "}
+          {errors.title && (
+            <ErrorMessage
+              message={errors.title}
+              onClose={() => setErrors({ ...errors, title: undefined })}
+            />
+          )}
         </label>
+
         <input
           type="text"
           required
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="w-full mb-[18px] h-[45px] mt-1 pl-[18px] border border-gray-300 rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px"
+          className={`w-full mb-[18px] h-[45px] mt-1 pl-[18px] border rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px ${
+            errors.title ? "border-red-500" : "border-gray-300"
+          }`}
           placeholder="Nombre"
         />
-        <label className="text-customGrey font-open-sans text-14px font-normal leading-19px text-left">
-          Descripcion
+
+        <label className="text-customGrey font-open-sans text-14px font-normal leading-19px text-left mt-4">
+          Descripcion{" "}
+          {errors.description && (
+            <ErrorMessage
+              message={errors.description}
+              onClose={() => setErrors({ ...errors, description: undefined })}
+            />
+          )}
         </label>
         <textarea
-          placeholder="Description"
+          placeholder="Descripción"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="w-full h-[156px] mt-1 pl-[18px] pt-[9px] border border-gray-300 rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px"
+          className={`w-full h-[156px] mt-1 pl-[18px] pt-[9px] border rounded-md text-neutralGrey font-open-sans text-14px font-normal leading-19px ${
+            errors.description ? "border-red-500" : "border-gray-300"
+          }`}
         />
+
         <div className="flex gap-[10px] mt-[18px] justify-between">
           <Button onClick={onClose} title="Cancelar" customColor customLabel />
           <Button onClick={handleSave} title="Guardar" />
